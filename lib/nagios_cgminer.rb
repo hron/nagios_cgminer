860f72d9996027be15abeaa640cdbdc2b37e79b8
Aleksei Gusev <aleksei.gusev@gmail.com>
2013-12-01 16:55:01 +0300

    Implement hashrate pooling.

--- lib/nagios_cgminer.rb
+++ lib/nagios_cgminer.rb
@@ -1,5 +1,50 @@
+require 'nagios-plugin'
+require 'json'
+
 require "nagios_cgminer/version"
 
 module NagiosCgminer
-  # Your code goes here...
+  class Plugin < Nagios::Plugin
+    def run
+      @cgminer_port = 4028
+
+      @opts.banner = "Usage: nagios_cgminer [options] CGMINER_URL"
+      @opts.on("-P", "--port 4028", "Port of cgminer API") do |port|
+        @cgminer_port = port
+      end
+      @opts.parse!
+
+      @cgminer_url = ARGV[0]
+      unless @cgminer_url
+        puts @opts.help
+        exit(255)
+      end
+      @critical &&= @critical.to_f
+      @warning &&= @warning.to_f
+
+      socket = Socket.new Socket::AF_INET, Socket::SOCK_STREAM
+      socket.connect(Socket.pack_sockaddr_in(@cgminer_port, @cgminer_url))
+      socket.send(JSON.generate({command: "summary"}), 0)
+      message = ""
+      while (more = socket.recv(4096)) != ""
+        message += more
+      end
+      summary = JSON.parse(message.strip)
+
+      average_hashrate = summary['SUMMARY'][0]['MHS av'].to_f
+      pool = "Unknown"
+      message = "Speed: #{average_hashrate}; Pool: #{pool}"
+      if average_hashrate < @critical
+        critical(message)
+      elsif average_hashrate < @warning
+        warning(message)
+      else
+        ok(message)
+      end
+    end
+
+    def cgminer_full_api_url
+      "#{@cgminer_url}:#{@cgminer_port}"
+    end
+  end
 end
--- nagios_cgminer.gemspec
+++ nagios_cgminer.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = NagiosCgminer::VERSION
   spec.authors       = ["Aleksei Gusev"]
   spec.email         = ["aleksei.gusev@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{Nagios plugin to monitor cgminer.}
+  spec.summary       = %q{Nagios plugin to monitor cgminer.}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "pry"
+
+  spec.add_dependency "nagios-plugin"
+  spec.add_dependency 'httparty'
 end
